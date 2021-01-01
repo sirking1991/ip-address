bc35b0f6ce1b32d27432433cd983c39a748896dc
sirking <sirking1991@gmail.com>
2021-01-01 13:05:13 +0800

    added list method

--- app/Classes/AuditLogService.php
+++ app/Classes/AuditLogService.php
@@ -6,6 +6,15 @@ use App\Models\AuditLog;
 
 class AuditLogService {
 
+    /** 
+     * returns list of recorded audit logs
+     */
+    public static function list($ip)
+    {
+        $return = AuditLog::with('user')->where('ip', $ip)->get();
+        return $return;
+    }
+
     public static function log($ip, $userId, $remarks) {
         AuditLog::create([
             'ip' => $ip,
